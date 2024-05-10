3bd43929715cd66a6311e08489a705c708aed983
Nic Pina <en.pina1@gmail.com>
2024-05-10 16:45:43 +0200

    202: Add csv functionality to FailedEntry class

--- app/models/failed_entry.rb
+++ app/models/failed_entry.rb
@@ -1,2 +1,12 @@
 class FailedEntry < ApplicationRecord
+  def self.to_csv
+    attributes = %w{List Seen Name Year IMDB} # CSV column names, added 'list_name'
+    CSV.generate(headers: true) do |csv|
+      csv << attributes # Adding the header row
+      all.includes(:list).each do |entry|
+        list_name = entry.list&.name || "No List" # Using safe navigation operator (&.) and providing a default
+        csv << [list_name, entry.completed, entry.name, entry.year, entry.imdb]
+      end
+    end
+  end
 end
