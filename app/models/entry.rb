cc97dd23676a73d781fe1ff9acf73bcbef9c7de2
Nic Pina <en.pina1@gmail.com>
2024-05-19 17:49:11 +0200

    #205: Refactor Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,13 +1,8 @@
-require "open-uri"
 require 'csv'
 
 class Entry < ApplicationRecord
   belongs_to :list
-  # validate :streamable
   validates :name, uniqueness: { scope: :list }
-
-  URL = "http://www.omdbapi.com/?"
-  API = ["&apikey=b64b5a87", "&apikey=eb34d99", "&apikey=a881ace5"]
 
   include PgSearch::Model
   pg_search_scope :search_by_input,
@@ -18,76 +13,14 @@ class Entry < ApplicationRecord
                     }
                   }
 
-  # def self.genres
-  #   Entry.all.group_by(&:genre).keys.map(&:split).flatten.map { |genre| genre.tr(',', '') }.uniq.sort
-  # end
+  after_create :check_source
 
-  def self.csv_to_movie(movie, user)
-    list = List.find_by(name: movie[:list]) unless movie[:list].empty?
-    entry = Entry.find_by(imdb: movie[:imdb], list: list) || Entry.find_by(name: movie[:title].strip, year: movie[:year], list: list)
-    if entry
-      puts "skipping: #{entry.name} (#{entry.year}) already exists"
-      return entry
-    end
-    imdb_id = movie[:imdb] || Entry.get_imdb(movie[:title], 1, movie[:year])&.first
-    raise Errors::NoResults, "No data could be found for#{movie[:title]} (#{movie[:year]})" if imdb_id.nil?
-
-    omdb_result = Entry.get_movie(imdb_id)
-    raise Errors::NoResults, "No data could be found for #{movie[:title]} (#{movie[:year]})" if omdb_result.nil?
-
-    p entry = Entry.create_movie(omdb_result) unless omdb_result.nil?
-    entry.list = list || List.create(name: movie[:list], user: user)
-    # entry.note = Faker::Lorem.paragraph
-    entry.category = 'Movie'
-    entry.completed = movie[:seen] == "TRUE"
-    entry.stream = entry.check_source
-    puts "saving: #{entry.name} (#{entry.year})"
-    entry.save!
-  rescue StandardError => e
-    puts "failed: #{e}"
-    FailedEntry.create(name: movie[:title], alt: movie[:alt], year: movie[:year])
-  end
-
-  def self.to_csv
-    attributes = %w{List Seen Name Year IMDB} # CSV column names, added 'list_name'
-    CSV.generate(headers: true) do |csv|
-      csv << attributes # Adding the header row
-      all.includes(:list).sort_by(&:year).each do |entry|
-        list_name = entry.list&.name || "No List" # Using safe navigation operator (&.) and providing a default
-        csv << [list_name, entry.completed, entry.name, entry.year, entry.imdb]
-      end
-    end
-  end
-
-  def self.get_imdb(title, number = 1, year = nil)
-    omdb_search = "#{URL}s=#{title.strip}#{API.sample}"
-    serialized_search = URI.parse(URI::Parser.new.escape(omdb_search)).open.read
-    response = JSON.parse(serialized_search)
-    return nil if response["Error"]
-
-    if year.nil?
-      selection = response["Search"].first(number)
-    else
-      selection = response["Search"].select { |hash| ((year.to_i - 1)..(year.to_i + 1)).include?(hash["Year"].to_i) }
-    end
-    selection.map { |movie| movie["imdbID"] }
-  end
-
-  def self.get_movie(imdb_id)
-    omdb_url = "#{URL}i=#{imdb_id}#{API.sample}"
-    serialized_title = URI.parse(omdb_url).open.read
-    result = JSON.parse(serialized_title)
-    return nil if result["Error"] || result["Poster"] == "N/A"
-
-    # return nil if result["Type"] != "movie" || result["Poster"] == "N/A"
-    return result
-  end
-
-  def self.create_movie(result)
-    Entry.new(
+  def self.create_from_OMDB(result, list, seen)
+    Entry.create!(
       media: result["Type"],
       source: "https://v2.vidsrc.me/embed/#{result["imdbID"]}",
       name: result["Title"],
+      alt: result["alt"],
       year: result["Year"].to_i,
       pic: result["Poster"],
       genre: result["Genre"],
@@ -99,24 +32,32 @@ class Entry < ApplicationRecord
       length: result["Runtime"].split(" ")[0].to_i,
       language: result["Language"],
       imdb: result["imdbID"],
-      completed: false
+      completed: seen == "TRUE",
+      list: list
     )
+  rescue StandardError => e
+    FailedEntry.create(name: result["Title"], year: result["Year"])
+    message = "Failed to create movie entry: #{e}"
+    Rails.logger.error(message)
+    return message
+  end
+
+  def self.to_csv
+    CsvExporterService.generate_csv(all)
+  end
+
+  def self.like(name)
+    where('name ILIKE ?', "%#{name}%").first
   end
 
   def check_source
-    url = source
-    begin
-      URI.open(url).read
-    rescue OpenURI::HTTPError
-      return false
-    end
-    return true
+    update(stream: UrlCheckerService.new(source).valid_source?)
   end
 
   def streamable
-    unless stream
-      update(source: '')
-      errors.add(:source, "is unavailable, do you have an alternative?")
-    end
+    return if stream
+
+    # update(source: '')
+    errors.add(:source, "is unavailable, do you have an alternative?")
   end
 end
