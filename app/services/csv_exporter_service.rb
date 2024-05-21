b63e14d5724f9d300f4cdf03d4497a73cee100b7
Nic Pina <en.pina1@gmail.com>
2024-05-21 21:30:18 +0200

    #220: Alter csv_export_service

--- app/services/csv_exporter_service.rb
+++ app/services/csv_exporter_service.rb
@@ -1,10 +1,23 @@
 require 'csv'
 
 class CsvExporterService
-  def self.generate_csv(entries)
-    attributes = %w[list seen name alt year imdb source] # CSV column names, added 'list_name'
+  def self.generate_seed_csv
+    attributes = Entry.attribute_names
     CSV.generate(headers: true) do |csv|
-      csv << attributes # Adding the header row
+      csv << (['seed', 'list_name'] + attributes) # Adding the header row
+      Entry.includes(:list).find_each do |entry|
+        list_name = entry.list.name
+        row = [true, list_name]
+        row << attributes.map { |attr| entry.send(attr) }
+        csv << row.flatten
+      end
+    end
+  end
+
+  def self.generate_user_csv(entries)
+    attributes = %w[list seen name alt year imdb source]
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
       entries.includes(:list).sort_by(&:year).each do |entry|
         list_name = entry.list.name
         row = [list_name, entry.completed, entry.name, entry.alt, entry.year, entry.imdb]
